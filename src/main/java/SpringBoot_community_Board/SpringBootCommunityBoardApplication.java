6aa3f3358083d3355fc65ca7e34566970604849c
JongwonKim0210 <71743704+JongwonKim0210@users.noreply.github.com>
2020-11-05 17:11:04 +0900

    start springboot

---
+++ src/main/java/SpringBoot_community_Board/SpringBootCommunityBoardApplication.java
@@ -0,0 +1,13 @@
+package SpringBoot_community_Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommunityBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommunityBoardApplication.class, args);
+	}
+
+}
--- src/test/java/SpringBoot_community_Board/SpringBootCommunityBoardApplicationTests.java
+++ src/test/java/SpringBoot_community_Board/SpringBootCommunityBoardApplicationTests.java
@@ -1,10 +1,10 @@
-package com.web.community;
+package SpringBoot_community_Board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CommunityApplicationTests {
+class SpringBootCommunityBoardApplicationTests {
 
 	@Test
 	void contextLoads() {
